e1e053313f1e410e4a05d3bb5debcd396f3931d7
mafkarafnanis22-png <afkarafnani57@gmail.com>
2025-10-16 13:35:40 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+//1. pilih tombol berdasarkan ID-nya
+const tombol = document.getElementById('websekolah')
+//2. tambahkan efek klik
+tombol.addEventListener('clik', function() {
+  alert('Halo !! Terimakasi sudah Berkunjung!');
+});
